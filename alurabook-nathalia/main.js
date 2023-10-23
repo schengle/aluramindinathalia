c51b02ac5c67f9b31784065c55dee3660aff4f73
schengle <130988496+schengle@users.noreply.github.com>
2023-10-23 12:27:16 -0300

    Add files via upload

--- alurabook-nathalia/main.js
+++ alurabook-nathalia/main.js
@@ -1,27 +1,51 @@
-function tocaSom(idElementAudio) { 
-    document.querySelector().play();
-}
-
-const ListaDeTeclas = document.querySelectorAll('.tecla');
-
-let contador = 0;
-
-//enquanto
-while(contador < listaDeTeclas.lenght)       { 
-
-   const tecla= listaDeTeclas[contador]
-   const instrumento = tecla.classList[1];
-   //template string
-const idAudio = `#som_${instrumento}`;
-
-//console.log(idAudio);
-
-tecla.onclick = function [] {
-    tocaSom(idAudio);
-}
-
-   contador = (contador + 1);
-
-   console.log(contador);
-}
-
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+  
+    if (elemento === null) {
+      alert('Elemento nao encontrado');
+    }
+  
+    if (elemento != null && elemento.localName === 'audio' ) {
+          elemento.play();
+    }   
+     else {
+        // alert('Elemento nao encontrado ou seletor invalido');
+        console.log('Elemento não encontrado ou seletor invalido');
+     }    
+  }
+  
+  
+  const ListaDeTeclas = document.querySelectorAll('.tecla');
+  
+  let contador = 0;
+  
+  
+  for (let contador = 0; contador < ListaDeTeclas.length; contador++) {
+  
+     const tecla = ListaDeTeclas[contador];
+     const instrumento = tecla.classList[1];
+  
+     const idAudio = `#som_${instrumento}`;
+  
+     console.log(idAudio);
+  
+     tecla.onclick = function () {
+        tocaSom(idAudio);
+     }
+  
+     tecla.onkeydown = function (evento) { 
+  
+        console.log(evento.code == 'Space')
+  
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+          tecla.classList.add('ativa');
+        }
+  
+     
+     }   
+     tecla.onkeyup = function ()  {
+        tecla.classList.remove ('ativa');
+  
+     }
+  
+  } 
